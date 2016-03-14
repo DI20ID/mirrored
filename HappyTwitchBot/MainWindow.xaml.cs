23c3a1534428d20ab1cb9bf5226bc37f610ae734
uberhalit <uberhalit@yandex.com>
2016-03-14 21:58:48 +0100

    formatting GUI "style" small additions

--- HappyTwitchBot/MainWindow.xaml.cs
+++ HappyTwitchBot/MainWindow.xaml.cs
@@ -1,56 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace HappyTwitchBot
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        internal ircClient irc = new ircClient();
+        internal string sUsername = "";
+        internal string sPassword = "";
 
-        
+
+        // entry point
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public ircClient irc = new ircClient();
 
-        private void tb_username_TextChanged(object sender, TextChangedEventArgs e)
-        {
+        #region EVENTHANDLER
 
-        }
-
-        private void tb_username_GotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox tb_username = (TextBox)sender;
-            tb_username.Text = string.Empty;
-            tb_username.GotFocus -= tb_username_GotFocus;
-        }
-
-        private void tb_password_GotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox tb_password = (TextBox)sender;
-            tb_password.Text = string.Empty;
-            tb_password.GotFocus -= tb_password_GotFocus;
-        }
-
+        // connect button click
         private void b_connect_Click(object sender, RoutedEventArgs e)
         {
-            irc = new ircClient("irc.twitch.tv", 6667, tb_username.Text, tb_password.Text);
+            sUsername = "secrethappyliooon";
+            sPassword = "oauth:fb54elmxy4hfrcasoph8rxux7blv4h";
+            //sUsername = tb_password.Text;
+            //sPassword = tb_password.Text;
+            irc = new ircClient("irc.twitch.tv", 6667, sUsername, sPassword);
             irc.joinRoom("happyliooon");
             /*while (true)
             {
@@ -62,16 +39,60 @@ namespace HappyTwitchBot
             }*/
         }
 
-        private void tb_channel_GotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox tb_channel = (TextBox)sender;
-            tb_channel.Text = string.Empty;
-            tb_channel.GotFocus -= tb_channel_GotFocus;
-        }
 
+        // button test Click
         private void testbutton_Click(object sender, RoutedEventArgs e)
         {
             irc.sendChatMessage("It works!");
         }
+
+
+        // textbox username TextChanged
+        private void tb_username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        // textbox username GotFocus
+        private void tb_username_GotFocus(object sender, RoutedEventArgs e)
+        {
+            /*
+            TextBox tb_username = (TextBox)sender;
+            tb_username.Text = string.Empty;
+            tb_username.GotFocus -= tb_username_GotFocus;
+            */
+        }
+
+
+        // textbox password GotFocus
+        private void tb_password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            /*
+            TextBox tb_password = (TextBox)sender;
+            tb_password.Text = string.Empty;
+            tb_password.GotFocus -= tb_password_GotFocus;
+            */
+        }
+
+
+        // textbox channel GotFocus
+        private void tb_channel_GotFocus(object sender, RoutedEventArgs e)
+        {
+            /*
+            TextBox tb_channel = (TextBox)sender;
+            tb_channel.Text = string.Empty;
+            tb_channel.GotFocus -= tb_channel_GotFocus;
+            */
+        }
+
+        #endregion
+
+
+        #region FUNCTIONS
+
+
+
+        #endregion
     }
 }
--- HappyTwitchBot/ircClient.cs
+++ HappyTwitchBot/ircClient.cs
@@ -1,62 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Net;
+﻿using System.IO;
 using System.Net.Sockets;
+
 
 namespace HappyTwitchBot
 {
     public class ircClient
     {
-            private string username;
-            private string channel;
+        private string username;
+        private string channel;
 
-            private TcpClient tcpClient;
-            private StreamReader inputStream;
-            private StreamWriter outputStream;
+        private TcpClient tcpClient;
+        private StreamReader inputStream;
+        private StreamWriter outputStream;
 
-            public ircClient()
-            { }
+        public ircClient()
+        { }
 
-            public ircClient(string ip, int port, string username, string password)
-            {
-                this.username = username;
-                tcpClient = new TcpClient(ip, port);
-                inputStream = new StreamReader(tcpClient.GetStream());
-                outputStream = new StreamWriter(tcpClient.GetStream());
+        public ircClient(string ip, int port, string username, string password)
+        {
+            this.username = username;
+            tcpClient = new TcpClient(ip, port);
+            inputStream = new StreamReader(tcpClient.GetStream());
+            outputStream = new StreamWriter(tcpClient.GetStream());
 
-                outputStream.WriteLine("PASS " + password);
-                outputStream.WriteLine("NICK " + username);
-                outputStream.WriteLine("USER " + username + " 8 * :" + username);
+            outputStream.WriteLine("PASS " + password);
+            outputStream.WriteLine("NICK " + username);
+            outputStream.WriteLine("USER " + username + " 8 * :" + username);
+        }
 
-            }
+        public void joinRoom(string channel)
+        {
+            this.channel = channel;
+            outputStream.WriteLine("JOIN #" + channel);
+            outputStream.Flush();
+        }
 
-            public void joinRoom(string channel)
-            {
-                this.channel = channel;
-                outputStream.WriteLine("JOIN #" + channel);
-                outputStream.Flush();
-            }
+        public void sendIrcMessage(string message)
+        {
+            outputStream.WriteLine(message);
+            outputStream.Flush();
+        }
 
-            public void sendIrcMessage(string message)
-            {
-                outputStream.WriteLine(message);
-                outputStream.Flush();
-            }
+        public void sendChatMessage(string message)
+        {
+            sendIrcMessage(":" + username + "!" + username + "@" + username + ".tmi.twitch.tv PRIVMSG #" + channel + " :" + message);
+        }
 
-            public void sendChatMessage(string message)
-            {
-                sendIrcMessage(":" + username + "!" + username + "@" + username + ".tmi.twitch.tv PRIVMSG #" + channel + " :" + message);
-            }
+        public string readMessage()
+        {
+            string message = inputStream.ReadLine();
+            return message;
+        }
 
-            public string readMessage()
-            {
-                string message = inputStream.ReadLine();
-                return message;
-            }
-        
-}
+    }
 }
