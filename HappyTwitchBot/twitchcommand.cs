87420eb5de6cf0e63631c424cd183102fd1a5d15
HappyLion <reinhard_m_b@hotmail.com>
2016-03-19 11:58:05 +0100

    added classes: twitchuser, twitchcommand

---
+++ HappyTwitchBot/twitchcommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTwitchBot
+{
+    public class twitchcommand
+    {
+        internal string main;
+        internal string parameter1;
+
+        internal string d_main;
+        internal string parameter2;
+
+        public twitchcommand(string main)
+        {
+            main = this.main;
+            this.parameter1 = "";
+            this.parameter2 = "";
+        }
+
+        public twitchcommand(string main, string parameter1)
+        {
+            this.main = main;
+            this.parameter1 = parameter1;
+            this.parameter2 = "";
+        }
+
+        public twitchcommand(string main, string parameter1, string parameter2)
+        {
+            this.main = main;
+            this.parameter1 = parameter1;
+            this.parameter2 = parameter2;
+        }
+    }
+}
---
+++ HappyTwitchBot/twitchuser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTwitchBot
+{
+    public class twitchuser
+    {
+
+        internal bool user;
+        internal bool moderator;
+        internal bool subscriber;
+
+        public twitchuser(bool user, bool moderator, bool subscriber)
+        {
+            user = this.user;
+            moderator = this.moderator;
+            subscriber = this.subscriber;
+        }
+    }
+}
